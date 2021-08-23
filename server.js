66bcf57b29a95938b62c24e29c03423454fa1d71
Moran Neptune <neptune@post.bgu.ac.il>
2021-08-23 16:02:38 +0300

    deploy

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 // Start the app by listening on the default Heroku port
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
